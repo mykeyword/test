87792bc62e293432aacba6aa95538bbf5b47721d
mykeyword <1352392500@qq.com>
2020-07-12 09:53:30 +0800

    2.创建工程

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,4 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private fun login(){
+        //登录功能
+    }
 }
